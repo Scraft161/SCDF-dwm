ff42a965caef8283a56f70e5ff42d08047e371f7
Scraft161 <scraft161@gmail.com>
2024-05-19 23:20:55 +0200

    add bar-height, multimon, and warp patches

--- patch.sh
+++ patch.sh
@@ -9,6 +9,9 @@ dwm-autostart-modified.diff
 dwm-flextile-20210722-138b405.diff
 dwm-winicon-6.3-v2.1.diff
 dwm-moveresize-20221210-7ac106c.diff
+dwm-multimon-4-status_all-6.4-modified.diff
+dwm-warp-6.4.diff
+dwm-bar-height-spacing-6.3-modified.diff
 dwm-ipc-6.5-modified.diff
 dwm-xresources-6.5-modified.diff
 config-SCDF-6.5.diff
@@ -23,4 +26,9 @@ fi
 for patch in $patchlist; do
 	echo "$patch"
 	patch -d "src/dwm-${dwmver}" -i "../../Patches/$patch"
+
+	if [ "$?" -ne 0 ]; then
+		printf '%s\n' "[ERR]: could not apply patch \`$patch\`, exiting!"
+		exit 1
+	fi
 done
